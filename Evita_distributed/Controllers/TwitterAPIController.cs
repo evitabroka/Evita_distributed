982a14619b56b0c99c5f3387351726b5fb9cdeaa
Evita Broka <evita.broka.e27076@mcast.edu.mt>
2020-06-16 19:53:00 +0200

    Commit for saved acces tokens

---
+++ Evita_distributed/Controllers/TwitterAPIController.cs
@@ -0,0 +1,36 @@
+﻿using Evita_distributed.Client;
+using Evita_distributed.Endpoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Evita_distributed.Controllers
+{
+    public class TwitterAPIController : Controller
+    {
+        protected Restclient AppClient = new Restclient();
+        private TwEndpoint Endpoint;
+
+        public TwitterAPIController() : base()
+        {
+            this.Endpoint = new TwEndpoint();
+        }
+
+        //[HttpGet]
+        //[Route("getFeed")]
+        //public FbFeedModel getFeed(string FbToken)
+        //{
+        //    Enpoiint.fBAccesToken = FbToken;
+        //    AppClient.endpoint = Enpoiint.GetFeedEndpoint();
+        //    JSONParser<FbFeedModel> JsonModel = new JSONParser<FbFeedModel>();
+        //    string values = AppClient.Request(HttpVerb.GET, Enpoiint.EndpointURL());
+        //    FbFeedModel Feedd = new FbFeedModel();
+        //    Feedd = JsonModel.parseJson(values);
+        //    return Feedd;
+
+        //}
+
+    }
+}
---
+++ Evita_distributed/Endpoints/TWEndPoint.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Evita_distributed.Endpoints
+{
+    public class TwEndpoint
+    {
+    }
+}
